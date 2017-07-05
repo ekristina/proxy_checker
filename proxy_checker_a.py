91d67f26720017de0633334e8dbf3e019eeab13f
ekristina <kyermolenko@lvivsoft.com>
2017-07-05 17:01:13 +0300

    initial commit

---
+++ proxy_checker_a.py
@@ -0,0 +1,51 @@
+import asyncio
+import csv
+
+import aiohttp
+
+WEBSITE = 'https://www.google.com'
+
+with open('proxy_timeouts.txt', 'w') as exit_file:
+
+    async def fetch(proxy):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(WEBSITE,
+                                       proxy=proxy):
+                    return proxy
+
+        except aiohttp.ClientConnectorError as e:
+            print('proxy', proxy, 'with ClientConnectorError: ', e)
+            if e.errno == 110:
+                exit_file.write(proxy + '\n')
+        except aiohttp.ServerDisconnectedError as e:
+            print('proxy', proxy, 'with ServerDisconnectedError: ', e)
+        except TimeoutError:
+            exit_file.write(proxy)
+        except Exception as e:
+            # exit_file.write(proxy)
+            print('bad proxy', proxy, e)
+
+    async def bound_fetch(proxy, sem):
+        # Getter function with semaphore.
+        async with sem:
+            await fetch(proxy)
+
+    async def main():
+        tasks = []
+        with open('proxylist.csv') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            sem = asyncio.Semaphore(1000)  # reduce concurrent request number
+            for row in spamreader:
+                if 'ip' in row:
+                    continue
+                # csv proxy format: user password ip port
+                proxy = 'http://' + row[2] + ':' + row[-1] + '@'+ row[0] + ':' + row[1]
+
+                task = asyncio.ensure_future(bound_fetch(proxy, sem))
+                tasks.append(task)
+
+            for r in asyncio.as_completed(tasks):
+                await r
+
+    parsed_data = asyncio.get_event_loop().run_until_complete(main())
